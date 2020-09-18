c85720f2e06694505e1f1c426a3340403ea7369a
tom-sartori <tom.sartori-letourneau@etu.umontpellier.frf>
2020-09-18 14:50:39 +0200

    fin ex 4

---
+++ TD2/Model.php
@@ -0,0 +1,19 @@
+<?php
+require_once 'Conf.php';
+
+class Model {
+	
+	public static $pdo; 
+
+
+	public static function Init() { 
+		  $hostname =  Conf::getHostname(); 
+		  $database_name = Conf::getDatabase(); 
+		  $login = Conf::getLogin();
+		  $password = Conf::getPassword(); 
+
+		self::$pdo = new PDO("mysql:host=$hostname;dbname=$database_name",$login,$password);
+	}
+}
+Model::Init(); 
+?>
---
+++ TD2/testModel.php
@@ -0,0 +1,5 @@
+<?php
+require_once "Model.php";
+echo "Connexion réussie !" ;
+?>
+
