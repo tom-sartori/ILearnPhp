9014f699dbf7b9e3f2e2967a2bbe483b2ca79289
tom-sartori <tom.sartori-letourneau@etu.umontpellier.frf>
2020-09-18 15:04:28 +0200

    fin ex5

--- TD2/Model.php
+++ TD2/Model.php
@@ -1,4 +1,5 @@
 <?php
+
 require_once 'Conf.php';
 
 class Model {
@@ -7,12 +8,30 @@ class Model {
 
 
 	public static function Init() { 
-		  $hostname =  Conf::getHostname(); 
-		  $database_name = Conf::getDatabase(); 
-		  $login = Conf::getLogin();
-		  $password = Conf::getPassword(); 
+		$hostname =  Conf::getHostname(); 
+		$database_name = Conf::getDatabase(); 
+		$login = Conf::getLogin();
+		$password = Conf::getPassword(); 
 
-		self::$pdo = new PDO("mysql:host=$hostname;dbname=$database_name",$login,$password);
+/*
+		try {
+			self::$pdo = new PDO("mysql:host=$hostname;dbname=$database_name",$login,$password);
+		} catch(PDOException $e) {
+			echo $e->getMessage();
+			die(); 
+		}
+*/
+
+		// Connexion à la base de données            
+		// Le dernier argument sert à ce que toutes les chaines de caractères 
+		// en entrée et sortie de MySql soit dans le codage UTF-8
+		self::$pdo = new PDO("mysql:host=$hostname;dbname=$database_name", $login, $password,
+			array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+
+		// On active le mode d'affichage des erreurs, et le lancement d'exception en cas d'erreur
+		self::$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+
 	}
 }
 Model::Init(); 
