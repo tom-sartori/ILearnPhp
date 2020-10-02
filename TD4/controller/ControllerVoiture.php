26d91af1f4676192fa7b9155f0a3f4ba39b1f197
tom-sartori <tom.sartori-letourneau@etu.umontpellier.frf>
2020-10-02 14:43:30 +0200

    fin ex 4

--- TD4/controller/ControllerVoiture.php
+++ TD4/controller/ControllerVoiture.php
@@ -1,5 +1,21 @@
+
 <?php
+
+require_once ('../model/ModelVoiture.php'); // chargement du modèle
+class ControllerVoiture {
+    public static function readAll() {
+        $tab_v = ModelVoiture::getAllVoitures();     //appel au modèle pour gerer la BD
+        require ('../view/voiture/list.php');  //"redirige" vers la vue
+    }
+}
+
+
+
+
+/* Ancien 
 require_once ('../model/ModelVoiture.php'); // chargement du modèle
 $tab_v = ModelVoiture::getAllVoitures();     //appel au modèle pour gerer la BD
 require ('../view/voiture/list.php');  //redirige vers la vue
+*/
+
 ?>
---
+++ TD4/controller/routeur.php
@@ -0,0 +1,4 @@
+<?php
+require_once 'ControllerVoiture.php';
+ControllerVoiture::readAll(); // Appel de la méthode statique $action de ControllerVoiture
+?>
