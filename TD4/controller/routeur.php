fdae7e8ab7a440428b6da7e7aee07e5a82a4b4ec
tom-sartori <tom.sartori-letourneau@etu.umontpellier.frf>
2020-10-02 14:50:21 +0200

    fin ex 5

--- TD4/controller/routeur.php
+++ TD4/controller/routeur.php
@@ -1,4 +1,10 @@
 <?php
 require_once 'ControllerVoiture.php';
-ControllerVoiture::readAll(); // Appel de la méthode statique $action de ControllerVoiture
+// On recupère l'action passée dans l'URL
+$action = $_GET['action'];
+// Appel de la méthode statique $action de ControllerVoiture
+ControllerVoiture::$action(); 
+
+// Avec readAll : 
+// http://webinfo.iutmontp.univ-montp2.fr/~sartorit/TD-PHP/TD4/controller/routeur.php?action=readAll
 ?>
