87590a63922e8f57a01a9d7513870d5743a37032
tom-sartori <tom.sartori-letourneau@etu.umontpellier.frf>
2020-10-14 13:32:29 +0200

    fin ex 6

--- TD5/view/voiture/list.php
+++ TD5/view/voiture/list.php
@@ -11,10 +11,11 @@
             //echo '<p> Voiture d\'immatriculation ' . $v->getImmatriculation() . '.</p>';
 
     	$imat = htmlspecialchars($v->getImmatriculation()); 
+    	$imat2 = rawurlencode($v -> getImmatriculation()); 
 
-        echo '<p> Voiture d\'immatriculation ' . '<a href="./index.php?action=read&Immatriculation=' . $imat . '">' . $imat . '. </a>' . 
+        echo '<p> Voiture d\'immatriculation ' . '<a href="./index.php?action=read&Immatriculation=' . $imat2 . '">' . $imat . '. </a>' . 
 
-        '<a href="./index.php?action=delete&immatriculation='. $imat .'">'.'Supprimer cette Voiture'.'</a>' . '.</p>';
+        '<a href="./index.php?action=delete&immatriculation='. $imat2 .'">'.'Supprimer cette Voiture'.'</a>' . '.</p>';
 
     }
     ?>
