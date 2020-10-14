ea8dd11913e12b9bf2566509bb4402512a39acd4
tom-sartori <tom.sartori-letourneau@etu.umontpellier.frf>
2020-10-14 16:59:46 +0200

    fin ex 8

--- TD5/view/view.php
+++ TD5/view/view.php
@@ -1,16 +1,28 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="UTF-8">
-        <title><?php echo $pagetitle; ?></title>
-    </head>
-    <body>
-<?php
+<head>
+	<meta charset="UTF-8">
+	<title><?php echo $pagetitle; ?></title>
+
+
+	<nav style="border: 1px solid navy; text-align:center; padding: 5px ">
+		<a href="index.php?action=readAll"> Liste des voitures </a> 
+		<a href="index.php?action=readAll&controller=utilisateur"> Liste des utilisateurs </a> 
+		<a href="index.php?action=readAll&controller=trajet"> Liste des trajets </a> 
+	</nav>
+</head>
+<body>
+	<?php
 // Si $controleur='voiture' et $view='list',
 // alors $filepath="/chemin_du_site/view/voiture/list.php"
-$filepath = File::build_path(array("view", $controller, "$view.php"));
-require $filepath;
-?>
-    </body>
+	$filepath = File::build_path(array("view", $controller, "$view.php"));
+	require $filepath;
+	?>
+</body>
+
+<footer>
+	<p style="border: 1px solid navy;text-align:right;padding-right:1em;">
+		Site de covoiturage pour covoitureurs.	</p>
+</footer>
 </html>
 
