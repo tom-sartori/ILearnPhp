183ef3a9e2123046ce70240be66fcaa3e869b645
Tom SARTORI <tom.sartori-letourneau@etu.umontpellier.fr>
2020-11-12 19:52:48 +0100

    TD7 ex3

--- TD7/view/view.php
+++ TD7/view/view.php
@@ -10,7 +10,7 @@
 		<a href="index.php?action=readAll&controller=utilisateur"> Liste des utilisateurs </a> 
 		<a href="index.php?action=readAll&controller=trajet"> Liste des trajets </a> 
 	</nav>
-    <link href="../personalisation.php">
+    <link href="../preference.html">
 </head>
 <body>
 	<?php
