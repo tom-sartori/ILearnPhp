1fc8fdbbe8b5b5dc593ce173052e7dd50b131205
Tom SARTORI <tom.sartori-letourneau@etu.umontpellier.fr>
2020-11-12 19:13:21 +0100

    up

--- TD7/session.php
+++ TD7/session.php
@@ -11,4 +11,9 @@ $tab = array(
     echo $_SESSION['login'];
     echo $_SESSION['tab'];
 
+
+    session_unset();     // unset $_SESSION variable for the run-time
+    session_destroy();   // destroy session data in storage
+    // Il faut réappeler session_start() pour accéder de nouveau aux variables de session
+    setcookie(session_name(),'',time()-1); // deletes the session cookie containing the session ID
 ?>
