52fee90f0a50108ce5fb8348892475baa63fdeb3
Tom SARTORI <tom.sartori-letourneau@etu.umontpellier.fr>
2020-11-12 16:13:26 +0100

    up

---
+++ testGit.php
@@ -0,0 +1,14 @@
+<?php 
+echo "Testt"; 
+//$out = shell_exec(ls -l); 
+//echo $out; 
+$outputs =  shell_exec('git pull https://tom-sartori:Githubconte2.0@github.com:tom-sartori/TD-PHP.git master');
+echo "<pre>$outputs</pre>";
+
+echo "<br>"; 
+//$out = shell_exec(ls -l); 
+//echo $out; 
+echo "Testt"; 
+
+?>
+
