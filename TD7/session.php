f6b211be0ee36b16ce240a4b4780186a8184c788
Tom SARTORI <tom.sartori-letourneau@etu.umontpellier.fr>
2020-11-12 19:10:52 +0100

    up

--- TD7/session.php
+++ TD7/session.php
@@ -1,8 +1,14 @@
 <?php
     session_start();
 
+$tab = array(
+    "un" => "oui",
+    "deux" => "non");
+
     $_SESSION['login'] = 'tom';
+    $_SESSION['tab'] = $tab;
 
     echo $_SESSION['login'];
+    echo $_SESSION['tab'];
 
 ?>
