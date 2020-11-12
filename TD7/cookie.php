4c60c22e976a6730f4e6e3d9dad875ef8be07013
Tom SARTORI <tom.sartori-letourneau@etu.umontpellier.fr>
2020-11-12 18:44:12 +0100

    up

--- TD7/cookie.php
+++ TD7/cookie.php
@@ -1,10 +1,12 @@
 <?php
-setcookie("TestCookie", "OK", time()+3600);  /* expire dans 1 heure = 3600 secondes */
 
 $tab = array("oui", "non");
 
-echo $_COOKIE[serialize($tab)];
+setcookie("TestCookie", serialize($tab), time()+3600);  /* expire dans 1 heure = 3600 secondes */
 
-//setcookie ("TestCookie", "", time() - 1);
+
+echo $_COOKIE["TestCookie"];
+
+setcookie ("TestCookie", "", time() - 1);
 
 ?>
